84b307dde3f188df5f15b955ee458f9185d9e6f3
TaeWooKang <rkdtjsdn123@naver.com>
2021-09-30 22:23:25 +0900

    task: 7.weiredString 추가

---
+++ src/7.weiredString.js
@@ -0,0 +1,36 @@
+//문제: https://im-developer.tistory.com/36?category=831367
+
+function getWeiredString (sentence) {
+  let tempSentence = ""
+  let tempIndex = 0
+  if (sentence) {
+    for (let char of sentence) {
+      if (char === " ") {
+        tempSentence = tempSentence + char
+        tempIndex = 0
+      } else {
+        if (tempIndex % 2 === 0) {
+          tempSentence = tempSentence + char.toUpperCase()
+        } else {
+          tempSentence = tempSentence + char.toLowerCase()
+        }
+        tempIndex++
+      }
+    }
+  }
+  return tempSentence
+}
+
+const items = [
+  "",//=> ""
+  null,//=> ""
+  undefined,//=> ""
+  "String",//=> returns "StRiNg"
+  "Weird string case" //=> returns "WeIrD StRiNg CaSe"
+];
+console.time("#1")
+for (let item of items) {
+  console.log(getWeiredString(item)
+  );
+}
+console.timeEnd("#1")
