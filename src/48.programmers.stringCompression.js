42ee3ce738c040cd670d137358ee3936307e6182
TaeWooKang <rkdtjsdn123@naver.com>
2021-10-27 22:24:46 +0900

    task: 48.programmers.stringCompression 추가

---
+++ src/48.programmers.stringCompression.js
@@ -0,0 +1,45 @@
+//문제: https://programmers.co.kr/learn/courses/30/lessons/60057
+
+function stringCompression (s) {
+  let answer = s.length;
+
+  for (let i = 1; i < s.length; i++) {
+    let tempSentense = ""
+    let tempChar = ""
+    let tempNum = 0
+    for (let j = i; j <= s.length + i; j += i) {
+      const currentChar = s.slice(j - i, j)
+      if (tempChar !== currentChar) {
+        if (tempNum === 1) {
+          tempSentense += tempChar
+        }
+        else if (tempNum > 1) {
+          tempSentense += `${tempNum}${tempChar}`
+        }
+        tempNum = 1
+      } else {
+        tempNum++
+      }
+      tempChar = currentChar
+    }
+    tempSentense += s.slice(s.length - s.length % i)
+
+    answer = Math.min(answer, tempSentense.length)
+  }
+
+  return answer;
+}
+
+const items = [
+  "aabbaccc", // =>	7
+  "ababcdcdababcdcd", // =>	9
+  "abcabcdede", // =>	8
+  "abcabcabcabcdededededede", // =>	14
+  "xababcdcdababcdcd", // =>	17
+]
+console.time("#1")
+for (let item of items) {
+  console.log(stringCompression(item)
+  );
+}
+console.timeEnd("#1")
