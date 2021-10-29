ebf4f17e0c23caade727ffe8dd2e8c4d63ade474
TaeWooKang <rkdtjsdn123@naver.com>
2021-10-29 09:36:16 +0900

    task: 58.programmers.findKimSeobangInSeoul

---
+++ src/58.programmers.findKimSeobangInSeoul.js
@@ -0,0 +1,17 @@
+//문제: https://programmers.co.kr/learn/courses/30/lessons/12917
+
+function findKimSeobangInSeoul (seoul) {
+  return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
+}
+
+
+const items = [
+  ["Jane", "Kim"] // =>	"김서방은 1에 있다"
+
+]
+console.time("#1")
+for (let item of items) {
+  console.log(findKimSeobangInSeoul(item)
+  );
+}
+console.timeEnd("#1")
