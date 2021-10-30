cf6677bd316ab9ace0c5b10ab8441b988bf95013
TaeWooKang <rkdtjsdn123@naver.com>
2021-10-30 13:50:52 +0900

    task: 69.programmers.evenAndOdd 추가

---
+++ src/69.programmers.evenAndOdd.js
@@ -0,0 +1,19 @@
+//문제: https://programmers.co.kr/learn/courses/30/lessons/12937?language=javascript
+
+function evenAndOdd (num) {
+  return Math.abs(num % 2) === 0 ? "Even" : "Odd"
+}
+
+
+const items = [
+  3, // =>	"Odd"
+  4, // => "Even"
+
+
+]
+console.time("#1")
+for (let item of items) {
+  console.log(evenAndOdd(item)
+  );
+}
+console.timeEnd("#1")
