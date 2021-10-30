bd67f0ed591c39e98467c7e30e68ba5baab10c31
TaeWooKang <rkdtjsdn123@naver.com>
2021-10-30 20:47:04 +0900

    task: 72.programmers.average 추가

---
+++ src/72.programmers.average.js
@@ -0,0 +1,18 @@
+//문제: https://programmers.co.kr/learn/courses/30/lessons/12937?language=javascript
+
+function average (arr) {
+  return arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
+}
+
+
+const items = [
+  [1, 2, 3, 4], // =>	2.5
+  [5, 5], // =>	5
+
+]
+console.time("#1")
+for (let item of items) {
+  console.log(average(item)
+  );
+}
+console.timeEnd("#1")
