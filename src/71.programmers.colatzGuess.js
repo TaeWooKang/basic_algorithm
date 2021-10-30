32d0ba23cc697a9c88b38614959b05940b11cf4d
TaeWooKang <rkdtjsdn123@naver.com>
2021-10-30 20:43:45 +0900

    task: 71.programmers.colatzGuess 추가

---
+++ src/71.programmers.colatzGuess.js
@@ -0,0 +1,25 @@
+//문제: https://programmers.co.kr/learn/courses/30/lessons/12937?language=javascript
+
+function colatzGuess (num) {
+  let temp = num
+  let answer = 0;
+  while (temp !== 1 && answer < 500) {
+    temp = temp % 2 === 0 ? temp / 2 : (temp * 3) + 1
+    answer++
+  }
+  return temp !== 1 ? -1 : answer;
+}
+
+
+const items = [
+  6, // => 8
+  16, // => 4
+  626331, // => -1
+
+]
+console.time("#1")
+for (let item of items) {
+  console.log(colatzGuess(item)
+  );
+}
+console.timeEnd("#1")
