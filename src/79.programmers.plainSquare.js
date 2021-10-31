58106e22ec44aa62b6f6e1adbff79b5573e28e7d
TaeWooKang <rkdtjsdn123@naver.com>
2021-10-31 16:48:01 +0900

    fix: 79번 예제 수정

--- src/79.programmers.plainSquare.js
+++ src/79.programmers.plainSquare.js
@@ -9,7 +9,7 @@ function plainSquare (record) {
 
 
 const items = [
-  ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"] // =>	["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
+  [8, 12] // =>	80
 
 ]
 console.time("#1")
