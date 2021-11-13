82409f4607bc3e5737d0130e55e5d7a9114ccc0a
TaeWooKang <rkdtjsdn123@naver.com>
2021-11-14 01:18:58 +0900

    task: 94.programmers.findDecimals 추가

---
+++ src/94.programmers.findDecimals.js
@@ -0,0 +1,52 @@
+// 문제: https://programmers.co.kr/learn/courses/30/lessons/42839
+
+function findDecimals (numbers) {
+  const answer = new Set();
+
+  getNumbers(numbers.split("")).forEach(temp => {
+    if (check(temp) && temp > 1) {
+      answer.add(+temp)
+    }
+  })
+  return answer.size;
+}
+
+function getNumbers (numbers, tempNumber = "") {
+  const result = []
+  if (numbers.length > 0) {
+    for (let i = 0; i < numbers.length; i++) {
+      const number = tempNumber + numbers[i]
+      const newNumbers = [...numbers]
+      newNumbers.splice(i, 1)
+      result.push(number)
+      result.push(...getNumbers(newNumbers, number))
+    }
+  }
+  return result
+}
+
+function check (num) {
+  if (num < 2) {
+    return false
+  }
+  for (let i = 2; i ** 2 <= num; i++) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+
+
+const items = [
+  "17", // =>	3
+  "011", // =>	2
+
+
+]
+console.time("#1")
+for (let item of items) {
+  console.log(findDecimals(item)
+  );
+}
+console.timeEnd("#1")
