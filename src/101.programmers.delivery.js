02e8359994a8e88daa7b16b4e7c69b85d11e3193
TaeWooKang <rkdtjsdn123@naver.com>
2021-11-23 01:09:41 +0900

    task: 101 오탈자수정

--- src/101.programmers.delivery.js
+++ src/101.programmers.delivery.js
@@ -1,7 +1,7 @@
 // 문제: https://programmers.co.kr/learn/courses/30/lessons/12978
 // 해답: https://velog.io/@leeeunbin/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%B0%B0%EB%8B%AC-JavaScript
 
-function delivery (relation) {
+function delivery (N, road, K) {
   let arr = Array(N + 1).fill(Infinity);
   let adj = Array.from(Array(N + 1), () => Array());
 
@@ -28,13 +28,14 @@ function delivery (relation) {
 }
 
 const items = [
-
+  [5, [[1, 2, 1], [2, 3, 3], [5, 2, 2], [1, 4, 2], [5, 3, 1], [5, 4, 2]], 3], // =>	4
+  [6, [[1, 2, 1], [1, 3, 2], [2, 3, 2], [3, 4, 3], [3, 5, 2], [3, 5, 3], [5, 6, 1]], 4] // =>	4
 
 ]
 
 console.time("#1")
 for (let item of items) {
-  console.log(delivery(item)
+  console.log(delivery(item[0], item[1], item[2])
   );
 }
 console.timeEnd("#1")
