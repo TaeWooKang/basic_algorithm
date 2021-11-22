a44f700efb1020bde7170cb44f7361e341a12520
TaeWooKang <rkdtjsdn123@naver.com>
2021-11-22 23:41:10 +0900

    task: 101.programmers.delivery 문제 추가

---
+++ src/101.programmers.delivery.js
@@ -0,0 +1,17 @@
+// 문제: https://programmers.co.kr/learn/courses/30/lessons/72412
+
+function delivery (relation) {
+
+}
+
+const items = [
+
+
+]
+
+console.time("#1")
+for (let item of items) {
+  console.log(delivery(item)
+  );
+}
+console.timeEnd("#1")
