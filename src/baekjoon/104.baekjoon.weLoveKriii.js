12a589a3847767467e1b6b7bd3ba54db32844c3c
TaeWooKang <rkdtjsdn123@naver.com>
2021-11-29 20:19:19 +0900

    task: 104.baekjoon.weLoveKriii 추가

--- src/baekjoon/104.baekjoon.weLoveKriii.js
+++ src/baekjoon/104.baekjoon.weLoveKriii.js
@@ -1,7 +1,9 @@
 // 문제: https://www.acmicpc.net/problem/2557
 
-function helloWorld () {
-    return "Hello World!"
+  function weLoveKriii () {
+    return `강한친구 대한육군
+강한친구 대한육군
+`
   }
   
   const items = [
@@ -10,7 +12,7 @@ function helloWorld () {
   ]
   console.time("#1")
   for (let item of items) {
-    console.log(helloWorld(item)
+    console.log(weLoveKriii(item)
     );
   }
   console.timeEnd("#1")
