3c0760065d439475a79748ca053fd12d5c2b6c0c
TaeWooKang <rkdtjsdn123@naver.com>
2021-12-25 14:40:41 +0900

    task: 123 로직 수정

--- src/programmers/123.programmers.gather.js
+++ src/programmers/123.programmers.gather.js
@@ -1,14 +1,7 @@
 // 문제: https://programmers.co.kr/learn/courses/30/lessons/84512
 
 function gather (word) {
-  let answer = 0;
-  let numList = { "A": 0, "E": 1, "I": 2, "O": 3, "U": 4 }
-  let startNum = [781, 156, 31, 6, 1]
-  for (let i = 0; i < word.length; i++) {
-    answer += startNum[i] * numList[word[i]] + 1
-  }
-
-  return answer;
+  return word.split("").reduce((acc, char, index) => acc + [781, 156, 31, 6, 1][index] * ("AEIOU".indexOf(char)) + 1, 0)
 }
 
 const items = [
