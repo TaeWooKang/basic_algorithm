1b48a7251e3f2d21ee892f714e5d0d0923b12994
TaeWooKang <rkdtjsdn123@naver.com>
2022-01-31 17:47:48 +0900

    fix: 148 BigInt 적용

--- src/baekjoon/148.baekjoon.bigNumberPlus.js
+++ src/baekjoon/148.baekjoon.bigNumberPlus.js
@@ -8,35 +8,10 @@ const rl = readline.createInterface({
 });
 
 rl.on('line', function(line) {
-  let result = []
-  let hasUpper = 0
+  const spliitedLine = line.split(" ").map((el) => BigInt(el))
+  const result = spliitedLine[0] + spliitedLine[1]
 
-  const spliitedString = line.split(" ")
-  const num1Array = spliitedString[0].split("").reverse()
-  const num2Array = spliitedString[1].split("").reverse()
-
-  const maxLength = Math.max(num1Array.length, num2Array.length)
-
-  for (let i = 0; i < maxLength; i++) {
-    const num1Number = Number(num1Array[i]) || 0
-    const num2Number = Number(num2Array[i]) || 0
-    let total = num1Number + num2Number + hasUpper
-    if (total > 9) {
-      hasUpper = 1
-      total = total - 10
-    } else {
-      hasUpper = 0
-    }
-    result.push(total)
-  }
-
-  result = result.reverse().join("")
-
-  if (hasUpper) {
-    result = "1" + result
-  }
-
-  console.log(result)
+  console.log(result.toString())
   rl.close()
 }).on("close", function() {
   process.exit();
