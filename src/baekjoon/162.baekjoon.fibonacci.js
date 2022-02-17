721f1a213865cceb842be62c8a2d11f04c5a1d5b
TaeWooKang <rkdtjsdn123@naver.com>
2022-02-17 20:19:19 +0900

    task: 162.baekjoon.fibonacci 추가

---
+++ src/baekjoon/162.baekjoon.fibonacci.js
@@ -0,0 +1,25 @@
+// 문제: https://www.acmicpc.net/problem/10872
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function (line) {
+  const num = +line
+  const max = num + 1
+
+  const result = [0, 1]
+
+  while(result.length < max) {
+    result.push(result[result.length-2] + result[result.length-1])
+  }
+
+  console.log(result[num])
+
+  rl.close()
+}).on("close", function () {
+  process.exit();
+});
